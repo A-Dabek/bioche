48bef8f1236be2c4ade420e4fb0915dd9068bcdc
Adrian Dąbek <adrian950113@gmail.com>
2018-11-13 19:01:36 +0100

    something to start with

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="posts">s
+      <fbl-post v-for="(post, index) of posts" v-bind:key="index" v-bind="post"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PostComponent from './components/Post.vue';
+
+export default Vue.extend({
+  name: 'app',
+  data: () => {
+    return {
+      posts: [
+        {author: 'Myszon Pyszon'},
+        {author: 'Dąbkowiak Bombkowiak'},
+      ]
+    } 
+  },
+  components: {
+    'fbl-post': PostComponent
+  }
+});
+</script>
+
+<style>
+#posts {
+  text-align: center
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="post">
+    <div id="header">
+      {{author}}
+    </div>
+    <div id="description">
+      {{description}}
+    </div>
+    <div id="content">
+      no content yet
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Post',
+  props: {
+    author: {type: String, default: 'default author'},
+    description: {type: String, default: 'default content'},
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
