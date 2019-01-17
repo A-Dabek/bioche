df8a04bc7d80e1c21b642e5bd9e26b4a6ae44b61
Adrian Dąbek <adrian.dabek@santander.pl>
2019-01-17 13:38:36 +0100

    liver healing

--- src/collection/organ/liver-organ.ts
+++ src/collection/organ/liver-organ.ts
@@ -2,11 +2,12 @@ import {OrganPlayable} from './organ-playable';
 import {FirebaseStatefulIcon} from '@/interface/firebase-stateful-icon';
 import {NumberStatefulIconSubState, StatefulIconSubState} from '@/interface/stateful-icon-sub-state';
 import {GameState} from '@/interface/game-state';
+import {HealthUpSubState} from '@/collection/sub-states/health-up.sub-state';
 
 export class LiverOrgan extends OrganPlayable {
 
   readonly sugar: StatefulIconSubState<number>;
-  // readonly health: NumberStatefulIconSubState;
+  readonly health: HealthUpSubState;
 
   getSubStates() {
     return [
@@ -17,12 +18,12 @@ export class LiverOrgan extends OrganPlayable {
 
   onTurnEnd(gameState: GameState): void {
     super.onTurnEnd(gameState);
-    this.health.changeValueBy(gameState, 1);
+    this.health.healUp(gameState);
   }
 
   constructor(state?: FirebaseStatefulIcon) {
     super("liver", state);
     this.sugar = new NumberStatefulIconSubState('cubes', 'sugar', state ? state.values['sugar'] || 10 : 10);
-    // this.health = new NumberStatefulIconSubState('health_up', 'health', state ? state.values['health'] : 10);
+    this.health = new HealthUpSubState(state ? state.values['health'] : 10);
   }
 }
--- src/collection/sub-states/health-up.sub-state.ts
+++ src/collection/sub-states/health-up.sub-state.ts
@@ -1,5 +1,30 @@
 import {NumberStatefulIconSubState} from '@/interface/stateful-icon-sub-state';
+import {GameState} from '@/interface/game-state';
 
 export class HealthUpSubState extends NumberStatefulIconSubState {
 
+  private pristine: boolean;
+
+  setValue(gameState: GameState, newValue: number): void {
+    super.setValue(gameState, newValue);
+    if (this.getValue() < this.startValue) this.pristine = false;
+  }
+
+  presentation(): { key: string; value: string; className: string } {
+    return {
+      ...super.presentation(),
+      key: this.pristine ? 'health_up' : super.presentation().key
+    };
+  }
+
+  healUp(gameState: GameState): void {
+    if (this.pristine) {
+      this.changeValueBy(gameState, 1)
+    }
+  }
+
+  constructor(private startValue: number) {
+    super('health_normal', 'health', startValue);
+    this.pristine = true;
+  }
 }
