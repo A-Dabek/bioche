ad1534e2f38a52fda2cda87fb7ec057915439df6
Adrian Dąbek <adrian.dabek@santander.pl>
2019-01-17 11:09:50 +0100

    liver stores sugar

--- src/collection/organ/liver-organ.ts
+++ src/collection/organ/liver-organ.ts
@@ -1,8 +1,32 @@
 import {OrganPlayable} from './organ-playable';
 import {FirebaseStatefulIcon} from '@/interface/firebase-stateful-icon';
+import {BasicStatefulIconSubState, StatefulIconSubState} from '@/interface/stateful-icon-sub-state';
 
 export class LiverOrgan extends OrganPlayable {
+
+  readonly sugar: StatefulIconSubState<number>;
+
+  getSubStates() {
+    return [
+      ...super.getSubStates(),
+      this.sugar
+    ];
+  }
+
+  getValues(): { [p: string]: any } {
+    return {
+      ...super.getValues(),
+      sugar: this.sugar.getValue()
+    };
+  }
+
   constructor(state?: FirebaseStatefulIcon) {
     super("liver");
+    if (state) {
+      this.sugar = new BasicStatefulIconSubState<number>('cubes', state.values['sugar']);
+    }
+    else {
+      this.sugar = new BasicStatefulIconSubState<number>('cubes', 10);
+    }
   }
 }
